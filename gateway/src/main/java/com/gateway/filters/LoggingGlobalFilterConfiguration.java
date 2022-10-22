50a7ca1698057e41f8cf794c53e91466370d04f1
Cesar Lopez <cesarlerdeut@gmail.com>
2022-10-22 12:12:27 +0200

    Comment Previous filters

--- gateway/src/main/java/com/gateway/filters/LoggingGlobalFilterConfiguration.java
+++ gateway/src/main/java/com/gateway/filters/LoggingGlobalFilterConfiguration.java
@@ -1,24 +1,23 @@
-package com.gateway.filters;
+// package com.gateway.filters;
 
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.cloud.gateway.filter.GlobalFilter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import lombok.extern.log4j.Log4j;
-import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Mono;
+// import lombok.extern.slf4j.Slf4j;
+// import reactor.core.publisher.Mono;
 
-@Slf4j
-@Configuration
-public class LoggingGlobalFilterConfiguration {
+// @Slf4j
+// @Configuration
+// public class LoggingGlobalFilterConfiguration {
 
-  @Bean
-  public GlobalFilter postGlobalFilter() {
-    return (exchange, chain) -> {
-      return chain.filter(exchange)
-      .then(Mono.fromRunnable(() -> {
-        log.info("~~~~~~~~~~~~~ Global Post filter executed ~~~~~~~~~~~~`");
-      }));
-    };
-  }
-}
+//   @Bean
+//   public GlobalFilter postGlobalFilter() {
+//     return (exchange, chain) -> {
+//       return chain.filter(exchange)
+//       .then(Mono.fromRunnable(() -> {
+//         log.info("~~~~~~~~~~~~~ Global Post filter executed ~~~~~~~~~~~~`");
+//       }));
+//     };
+//   }
+// }
--- gateway/src/main/java/com/gateway/filters/LoggingGlobalPreFilter.java
+++ gateway/src/main/java/com/gateway/filters/LoggingGlobalPreFilter.java
@@ -1,25 +1,25 @@
-/*
- * @author: Cesar Lopez
- * @copyright 2022 Wayfair LLC - All rights reserved
- */
-package com.gateway.filters;
+// /*
+//  * @author: Cesar Lopez
+//  * @copyright 2022 Wayfair LLC - All rights reserved
+//  */
+// package com.gateway.filters;
 
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
+// import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+// import org.springframework.cloud.gateway.filter.GlobalFilter;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.server.ServerWebExchange;
 
-import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Mono;
+// import lombok.extern.slf4j.Slf4j;
+// import reactor.core.publisher.Mono;
 
-@Slf4j
-@Component
-public class LoggingGlobalPreFilter implements GlobalFilter {
+// @Slf4j
+// @Component
+// public class LoggingGlobalPreFilter implements GlobalFilter {
 
-  @Override
-  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-    log.info("~~~~~~~~~~~~~ Global Pre filter executed ~~~~~~~~~~~~`");
-    return chain.filter(exchange);
-  }
+//   @Override
+//   public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//     log.info("~~~~~~~~~~~~~ Global Pre filter executed ~~~~~~~~~~~~`");
+//     return chain.filter(exchange);
+//   }
 
-}
+// }
